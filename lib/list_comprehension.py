2a2a87dfefbb0f0d28d73bfd3f67f8de18ad32af
Sam <samuel.waters@gmail.com>
2022-11-07 15:22:59 -0700

    finished

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-import pyautogui
+# import pyautogui
 
-pyautogui.moveRel(0, 50, duration = 1)
-pyautogui.moveRel(0, -150, duration = 1)
+# pyautogui.moveRel(0, 50, duration = 1)
+# pyautogui.moveRel(0, -150, duration = 1)
 
-def repeat_movement(num):
-    i = 0
-    while i < num:
-        pyautogui.moveRel(0, 50, duration = 1)
-        pyautogui.moveRel(0, -150, duration = 10)
-        i += 1
+# def repeat_movement(num):
+#     i = 0
+#     while i < num:
+#         pyautogui.moveRel(0, 50, duration = 1)
+#         pyautogui.moveRel(0, 150, duration = 10)
+#         pyautogui.moveTo(10, 10, 50)
+#         i += 1
+
 
 def return_evens(num_list):
-    pass
+    seq= []
+    for i in num_list:
+        if i % 2 == 0:
+            seq.append(i)
+    return seq
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = []
+    for i in sentence_list:
+        exclamation_list.append(i + "!")
+    return exclamation_list
+
 
 
 
