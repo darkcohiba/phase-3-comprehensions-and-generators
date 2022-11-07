df5e408f479980be42b02d8f996e9121ce2b92e4
Sam <samuel.waters@gmail.com>
2022-11-06 19:47:25 -0700

    moving mouse

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+import pyautogui
+
+pyautogui.moveRel(0, 50, duration = 1)
+pyautogui.moveRel(0, -150, duration = 1)
+
+def repeat_movement(num):
+    i = 0
+    while i < num:
+        pyautogui.moveRel(0, 50, duration = 1)
+        pyautogui.moveRel(0, -150, duration = 10)
+        i += 1
 
 def return_evens(num_list):
     pass
 
 def make_exclamation(sentence_list):
     pass
+
+
+
